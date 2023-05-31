d8614b87c736a8f397f215ccd0b842f1203f4dea
Amin eB <aminebslh@gmail.com>
2023-05-31 14:15:41 +0100

    [WORKING] 2 changes: 1. added cloud image display 2. added firebase course data display

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,23 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:medin/firebase_options.dart';
 import 'package:medin/view_models/login.viewmodel.dart';
 import 'package:medin/view_models/signup.viewmodel.dart';
-import 'package:medin/view_models/spinner.viewmodel.dart';
-import 'package:medin/views/courses.view.dart';
 import 'package:medin/views/login.view.dart';
-import 'package:medin/views/signup.view.dart';
-import 'package:medin/views/spinner.view.dart';
 import 'package:provider/provider.dart';
 import 'package:medin/utils/keys.dart';
 import 'package:medin/view_models/equipment.viewmodel.dart';
+import 'package:medin/view_models/course.viewmodel.dart';
 import 'package:medin/view_models/content.viewmodel.dart';
 import 'package:medin/repositories/equipment.repo.dart';
+import 'package:medin/repositories/course.repo.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseAppCheck.instance.activate(
+    webRecaptchaSiteKey: 'recaptcha-v3-site-key',
+    androidProvider: AndroidProvider.debug,
+  );
 
   runApp(MyApp());
 }
@@ -31,6 +34,9 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ListenableProvider<CourseRepo>(create: (context) => CourseRepoImpl()),
+        ListenableProvider<CourseViewModel>(
+            create: (context) => CourseViewModel()),
         ListenableProvider<EquipmentRepo>(
             create: (context) => EquipmentRepoImpl()),
         ListenableProvider<ContentViewModel>(
